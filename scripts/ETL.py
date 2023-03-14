79ce6e75df6fa7bb5464540f25cf925e5ef4cadc
Taylor Terry <taylor.terry@gmail.com>
2023-03-14 14:57:52 -0400

    add new incident source, update ETL and notebook

--- scripts/ETL.py
+++ scripts/ETL.py
@@ -16,14 +16,21 @@ ARREST_DATA = pd.read_csv("../data/raw/Adult_Arrests.csv.gz")
 INCIDENT_DATA = pd.concat(
     map(pd.read_csv, glob.glob("../data/raw/Crime_Incidents*")), ignore_index=True
 )
+INCIDENT_DATA_ALL = pd.read_csv("../data/raw/dc-crimes-search-results.csv")
 
 
 data_cleanup(STOP_DATA, "DATETIME").to_csv(
     "../data/clean/stop_data.csv.gz", index=False, compression="gzip"
 )
+
 data_cleanup(ARREST_DATA, "DATE_").to_csv(
     "../data/clean/arrest_data.csv.gz", index=False, compression="gzip"
 )
+
 data_cleanup(INCIDENT_DATA, "START_DATE").to_csv(
     "../data/clean/incident_data.csv.gz", index=False, compression="gzip"
+)
+
+data_cleanup(INCIDENT_DATA_ALL, "START_DATE").to_csv(
+    "../data/clean/incident_data_all.csv.gz", index=False, compression="gzip"
 )
